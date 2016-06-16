b9d3027e9c2026ad06964b17c3f45657a4ec9570
liangqingle <757431580@qq.com>
2016-06-16 11:25:05 +0800

    day one homework one

---
+++ oldboy/day/day1/homework_one.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+"""
+    作业二：编写登陆接口
+        输入用户名密码
+        认证成功后显示欢迎信息
+        输错三次后锁定
+"""
+
+from getpass import getuser, getpass
+
+checkcount = 3
+passflag = False
+usernamelist = ["one","two","three","four","five"]
+userpasswordlist = ["one","two","three","four","five"]
+
+while checkcount>0:
+    username_input = input("username:")
+    userpassword_input = getpass()
+    try:
+        print(userpasswordlist[usernamelist.index(userpassword_input)])
+
+        if userpassword_input == userpasswordlist[usernamelist.index(userpassword_input)]:
+            passflag=True
+            break
+    except Exception:
+        print("找不到密码")
+    checkcount-=1
+
+if passflag:
+    print("登陆成功")
+else:
+    print("登陆失败")    
